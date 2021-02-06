ba888b046d01cadfe4f40f94b0ba97fb0f62b8c0
y4shiro <y4shironao@gmail.com>
2021-02-06 11:45:18 +0900

    apply grid layout

---
+++ src/components/MessageInputField.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const MessageInputField = () => {
+  return <div>MessageInputField</div>;
+};
+
+export default MessageInputField;
+
---
+++ src/components/MessageList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const MessageList = () => {
+  return <div>MessageList</div>;
+};
+
+export default MessageList;
+

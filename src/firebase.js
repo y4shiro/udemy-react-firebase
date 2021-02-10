adb9a0cd2c8a9c6eb8447cf74e488af68dd9f6be
y4shiro <y4shironao@gmail.com>
2021-02-10 17:16:15 +0900

    set grid item

--- src/firebase.js
+++ src/firebase.js
@@ -11,8 +11,8 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('messages');
+const messagesRef = database.ref('messages');
 
 export const pushMessage = ({ name, text  }) => {
-  messageRef.push({ name, text });
+  messagesRef.push({ name, text });
 };

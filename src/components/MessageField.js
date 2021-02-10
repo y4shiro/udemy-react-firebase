9c949a2321658ac922e23c0d491b7265df3ce090
y4shiro <y4shironao@gmail.com>
2021-02-10 17:09:51 +0900

    create app

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
 
+import { pushMessage } from '../firebase';
+
 const MessageField = ({ name, setText, text }) => {
   const [isComposed, setIsComposed] = useState(false);
-  console.log({ text })
 
   return <TextField
     fullWidth={true}
@@ -12,7 +13,7 @@ const MessageField = ({ name, setText, text }) => {
       if (isComposed) return;
       if (e.target.value === '') return;
       if (e.key === 'Enter') {
-        console.log('push message to firebase');
+        pushMessage({ name: 'y4shiro', text });
         setText('');
         e.preventDefault();
       }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDRhQ9Bjo9o6dS7iwZnxsAiN0P131WjUA",
+  authDomain: "idobatakaigi-e82a7.firebaseapp.com",
+  projectId: "idobatakaigi-e82a7",
+  storageBucket: "idobatakaigi-e82a7.appspot.com",
+  messagingSenderId: "682729633863",
+  appId: "1:682729633863:web:f450ce813f48f6b2435962"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text  }) => {
+  messageRef.push({ name, text });
+};
